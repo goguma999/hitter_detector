5a080a9e71e89849f3f1362b1ef193a7c9282b2b
goguma999 <miny9501@gmail.com>
2024-10-31 10:35:50 +0900

    Update app_hitter.py

--- app_hitter.py
+++ app_hitter.py
@@ -26,38 +26,13 @@ st.markdown(
 # 커스텀 타이틀 표시
 st.markdown('<p class="custom-title">Who is the hitter</p>', unsafe_allow_html=True)
 
-# 사물 검출 버튼 추가
+# 사물 검출 버튼 추가  이걸 위쪽으로 올린거 
 if st.button("타자 분석하기"):     # 사물검출 실행이라는 버튼을 추가합니다. 버튼을 누르면 
     if uploaded_file is not None:  # 업로드된 영상이 있다면 
         st.session_state["processed_video"] = uploaded_file   # 검출된 영상을 사용
-        st.success("사물 검출이 완료되어 오른쪽에 표시됩니다.")  # 이 메세지 출력
+        st.success("타자 분석이 완료되어 오른쪽에 표시됩니다.")  # 이 메세지 출력
     else:
-        st.warning("사물 검출을 실행하려면 비디오 파일을 업로드하세요.")
-
-# 버튼 스타일을 CSS로 커스터마이즈
-st.markdown(
-    <style>
-    .custom-button {
-        display: flex;
-        justify-content: center; /* 버튼을 가운데로 정렬 */
-    }
-    .stButton > button {
-        background-color: #506EFD; /* 버튼 배경색 */
-        color: white; /* 버튼 글씨 색 */
-        padding: 0.5em 1em;
-        border: none;
-        border-radius: 0.3em;
-        font-weight: bold;
-        cursor: pointer;
-    }
-    .stButton > button:hover {
-        background-color: #4050c4; /* 버튼 호버 시 색상 */
-    }
-    </style>,
-    unsafe_allow_html=True
-)
-
-
+        st.warning("타자 분석을 실행하려면 비디오 파일을 업로드하세요.")
 
 # 전체 레이아웃을 컨테이너로 감싸기
 with st.container():     # with절로 하나의 기능을 하는 코드를 묶어줌.(가독성 높이기) 
@@ -69,17 +44,13 @@ with st.container():     # with절로 하나의 기능을 하는 코드를 묶�
     with col1:
         st.header("원본 영상")    # col1 영역의 제목
         if uploaded_file is not None:   # 영상이 업로드가 되었다면 
-            st.video(uploaded지 결과가 표시됩니다.</p>
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+            st.video(uploaded_file)     # 영상 플레이 해라~ 
+        else:
+            st.write("원본 영상을 표시하려면 비디오 파일을 업로드하세요.")
 
-
-
-
-
-
-
-
-
+    with col2:   
+        st.header("타자 분석 결과 영상")  # col2에 해당하는 영역의 제목 
+        if "processed_video" in st.session_state:     # 사물 검출 완료된 비디오가 있으면 
+            st.video(st.session_state["processed_video"])  # 그 비디오를 플레이 해라 
+        else:
+            st.write("여기에 타자 분석 결과가 표시됩니다.")
