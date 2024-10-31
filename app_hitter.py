ca5088452b42e3d23565f914718c68407678fec7
goguma999 <miny9501@gmail.com>
2024-10-31 10:13:50 +0900

    Update app_hitter.py

--- app_hitter.py
+++ app_hitter.py
@@ -4,8 +4,8 @@ import streamlit as st
 # 전체 레이아웃을 넓게 설정
 st.set_page_config(layout="wide")
 
-# 제목 설정
-st.title("Who is the hitter")
+# # 제목 설정
+# st.title("Who is the hitter")
 
 # 커스텀 CSS를 사용해 폰트 스타일 설정
 st.markdown(
