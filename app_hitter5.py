d0ffd28451dfb0ed291fdf40ab6334fda0a0fffc
goguma999 <miny9501@gmail.com>
2024-11-14 18:05:52 +0900

    Update app_hitter5.py

--- app_hitter5.py
+++ app_hitter5.py
@@ -74,7 +74,7 @@ with st.container():
             )
 
 # 페이지 하단에 결과 정보 미리 표시
-st.write("### 분석 결과: 결과: 영상 속 타자는 **_______** 입니다.")
+st.write("### 분석 결과: 영상 속 타자는 **_______** 입니다.")
 
 # "타자 분석 실행" 버튼이 클릭되었을 때 비디오 분석 시작
 if run_analysis and uploaded_file:
@@ -146,12 +146,12 @@ if run_analysis and uploaded_file:
     # 재인코딩된 비디오 다운로드 버튼 제공
     with open(reencoded_path, "rb") as file:
         st.download_button(
-            label="재인코딩된 결과 영상 다운로드",
+            label="결과 영상 다운로드",
             data=file,
             file_name=f"reencoded_video_{speed}x.mp4",
             mime="video/mp4"
         )
 
     # 분석 결과 표시
-    result_text = f"결과: 영상 속 타자는 **{frequent_detection if frequent_detection else '없음'}** 입니다."
+    result_text = f"영상 속 타자는 **{frequent_detection if frequent_detection else '없음'}** 입니다."
     st.write("### 분석 결과: " + result_text)
